a93a438b616982af7127f388ddb068dbc098f207
Kazuma Hatta <tori31001@gmail.com>
2015-05-13 10:39:46 +0900

    add README update batch

--- standalone-nwjs/make_webkitapp.bat
+++ standalone-nwjs/make_webkitapp.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
-del /Q nwjs-v0.12.0-win-x64\package.nw
-7z a -tzip nwjs-v0.12.0-win-x64\package.nw index.html
-7z a -tzip nwjs-v0.12.0-win-x64\package.nw package.json
-copy /Y conf.json nwjs-v0.12.0-win-x64\
-copy /b nwjs-v0.12.0-win-x64\nw.exe+nwjs-v0.12.0-win-x64\package.nw nwjs-v0.12.0-win-x64\standalone_display.exe
-start nwjs-v0.12.0-win-x64\standalone_display.exe
+del /Q nwjs\package.nw
+7z a -tzip nwjs\package.nw index.html
+7z a -tzip nwjs\package.nw package.json
+copy /Y conf.json nwjs\
+copy /b nwjs\nw.exe+nwjs\package.nw nwjs\standalone_display.exe
+start nwjs\standalone_display.exe
