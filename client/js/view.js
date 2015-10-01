2d524240723a8e4f1c6a40796af57b04608e5da6
KazumaHatta <tori31001@gmail.com>
2015-10-01 19:25:15 +0900

    remove unused files

--- client/js/view.js
+++ client/js/view.js
@@ -1,140 +1,180 @@
 /*jslint devel:true*/
-/*global io, socket, WebSocket, Blob, URL, FileReader, DataView, Uint8Array */
+/*global io, socket, WebSocket, Blob, URL, FileReader, DataView, Uint8Array, unescape, escape */
 
-(function (metabinary) {
+(function (vscreen, vsutil, connector) {
 	"use strict";
 
 	console.log(location);
-	var client = new WebSocket("ws://" + location.hostname + ":8081/v1/"),
-		updateType = "all";
+	var client,
+		reconnectTimeout = 2000,
+		updateType = "all",
+		timer,
+		windowData = null,
+		metaDataDict = {},
+		windowType = "window",
+		doneAddWindow,
+		doneGetWindow,
+		doneGetContent,
+		doneGetMetaData;
 	
 	/**
-	 * Description
-	 * @method onopen
+	 * メタデータが表示中であるかを判別する
+	 * @method isVisible
+	 * @param {JSON} metaData 判別対象メタデータ
+	 * @return LogicalExpression
 	 */
-	client.onopen = function () {
-		client.send("view");
-	};
+	function isVisible(metaData) {
+		console.log(metaData);
+		return (metaData.hasOwnProperty('visible') && (metaData.visible === "true" || metaData.visible === true));
+	}
 	
 	/**
-	 * Description
-	 * @method onclose
+	 * クライアントサイズを取得する
+	 * @method getWindowSize
+	 * @return ObjectExpression クライアントサイズ
 	 */
-	client.onclose = function () {
-		console.log('close');
-	};
-
+	function getWindowSize() {
+		return {
+			width : window.innerWidth,
+			height : window.innerHeight
+		};
+	}
+	
+	function fixedEncodeURIComponent(str) {
+		return encodeURIComponent(str).replace(/[!'()]/g, escape).replace(/\*/g, "%2A");
+	}
+	
+	function insertElementWithDictionarySort(area, elem) {
+		var i,
+			child,
+			isFoundIDNode = false;
+		if (!area.childNodes || area.childNodes.lendth === 0) {
+			area.appendChild(elem);
+			return;
+		}
+		for (i = 0; i < area.childNodes.length; i = i + 1) {
+			child = area.childNodes[i];
+			if (child.id && child.id.indexOf('_manip') < 0) {
+				if (elem.id < child.id) {
+					isFoundIDNode = true;
+					area.insertBefore(elem, child);
+					break;
+				}
+			}
+		}
+		if (!isFoundIDNode) {
+			area.appendChild(elem);
+			return;
+		}
+	}
+	
+	/**
+	 * cookie取得
+	 * @method getCookie
+	 * @param {String} key cookieキー
+	 * @return Literal cookieデータ
+	 */
+	function getCookie(key) {
+		var i,
+			pos,
+			cookies;
+		if (document.cookie.length > 0) {
+			console.log("all cookie", document.cookie);
+			cookies = [document.cookie];
+			if (document.cookie.indexOf(';') >= 0) {
+				cookies = document.cookie.split(';');
+			}
+			for (i = 0; i < cookies.length; i = i + 1) {
+				pos = cookies[i].indexOf(key + "=");
+				if (pos >= 0) {
+					return unescape(cookies[i].substring(pos + key.length + 1));
+				}
+			}
+		}
+		return "";
+	}
+	
+	/**
+	 * cookie保存
+	 * @method saveCookie
+	 */
+	function saveCookie() {
+		if (windowData) {
+			console.log("saveCookie", windowData);
+			document.cookie = 'window_id=' + windowData.id;
+			document.cookie = windowData.id + '_x=' + windowData.posx;
+			document.cookie = windowData.id + '_y=' + windowData.posy;
+			document.cookie = windowData.id + '_visible=' + windowData.visible;
+		}
+	}
+	
+	/**
+	 * View側Window[Display]登録
+	 * @method registerWindow
+	 */
+	function registerWindow() {
+		var wh = getWindowSize(),
+			cx = wh.width / 2.0,
+			cy = wh.height / 2.0,
+			window_id = getCookie('window_id'),
+			visible,
+			posx,
+			posy,
+			hashid = location.hash.split("#").join("");
+		
+		vscreen.assignWhole(wh.width, wh.height, cx, cy, 1.0);
+		
+		if (hashid.length > 0) {
+			window_id = decodeURIComponent(hashid);
+			visible = "true";
+			if (getCookie(window_id + '_visible') === 'false') {
+				visible = "false";
+			}
+			posx = getCookie(window_id + '_x');
+			posy = getCookie(window_id + '_y');
+			if (!posx) { posx = window.screenX || window.screenLeft; }
+			if (!posy) { posy = window.screenY || window.screenTop; }
+			connector.send('reqAddWindow', {id : window_id, posx : posx, posy : posy, width : wh.width, height : wh.height, visible : visible }, doneAddWindow);
+		} else if (window_id !== "") {
+			visible = (getCookie(window_id + '_visible') === "true");
+			posx = getCookie(window_id + '_x');
+			posy = getCookie(window_id + '_y');
+			connector.send('reqAddWindow', { id : window_id, posx : posx, posy : posy, width : wh.width, height : wh.height, visible : visible }, doneAddWindow);
+		} else {
+			connector.send('reqAddWindow', { posx : 0, posy : 0, width : wh.width, height : wh.height, visible : false }, doneAddWindow);
+		}
+	}
+	
 	/// update all contants
 	/**
-	 * Description
+	 * update contants.
 	 * @method update
 	 */
-	function update() {
+	function update(targetid) {
 		var previewArea = document.getElementById('preview_area');
 		
 		if (updateType === 'all') {
 			console.log("update all");
 			previewArea.innerHTML = "";
-			client.send(JSON.stringify({ command : 'GetContent', type: 'all', id: ''}));
+			connector.send('GetContent', { type: 'all', id: '' }, doneGetContent);
+		} else if (updateType === 'window') {
+			console.log("update winodow");
+			connector.send('GetWindow', { id : windowData.id}, doneGetWindow);
 		} else {
 			console.log("update transform");
-			client.send(JSON.stringify({ command : 'GetMetaData', type: 'all', id: ''}));
-		}
-	}
-	
-	/// initialize.
-	/// setup gui events
-	/**
-	 * Description
-	 * @method init
-	 */
-	function init() {
-	}
-	
-	window.onload = init;
-
-	/**
-	 * Description
-	 * @method resizeText
-	 * @param {} elem
-	 * @param {} rect
-	 */
-	function resizeText(elem, rect) {
-		var lineCount = 1,
-			fsize;
-		console.log("rect:", rect);
-
-		lineCount = elem.innerHTML.split("\n").length;
-		fsize = parseInt((parseInt(rect.h, 10) - 1) / lineCount, 10);
-		elem.style.fontSize = fsize + "px";
-		if (fsize < 9) {
-			elem.style.fontSize = "9px";
-			elem.style.overflow = "auto";
-		}
-		elem.style.width = rect.w + 'px';
-		elem.style.height = rect.h + 'px';
-	}
-	
-	/**
-	 * Description
-	 * @method makeRect
-	 * @param {} left
-	 * @param {} top
-	 * @param {} width
-	 * @param {} height
-	 * @return ObjectExpression
-	 */
-	function makeRect(left, top, width, height) {
-		return {
-			x : left,
-			y : top,
-			w : width,
-			h : height
-		};
-	}
-	
-	/**
-	 * Description
-	 * @method toIntRect
-	 * @param {Object} metaData
-	 * @return CallExpression
-	 */
-	function toIntRect(metaData) {
-		return makeRect(
-			parseInt(metaData.posx, 10),
-			parseInt(metaData.posy, 10),
-			parseInt(metaData.width, 10),
-			parseInt(metaData.height, 10)
-		);
-	}
-	
-	/**
-	 * Description
-	 * @method assignMetaData
-	 * @param {} elem
-	 * @param {Object} metaData
-	 */
-	function assignMetaData(elem, metaData) {
-		elem.style.left = Number(metaData.posx) + "px";
-		elem.style.top = Number(metaData.posy) + "px";
-		elem.style.width = Number(metaData.width) + "px";
-		elem.style.height = Number(metaData.height) + "px";
-		if (metaData.type === "text") {
-			resizeText(elem, toIntRect(metaData));
-		}
-		if (metaData.width < 10) {
-			elem.style.width = "";
-		}
-		if (metaData.height < 10) {
-			elem.style.height = "";
+			if (targetid) {
+				connector.send('GetMetaData', { type: '', id: targetid}, doneGetMetaData);
+			} else {
+				connector.send('GetMetaData', { type: 'all', id: ''}, doneGetMetaData);
+			}
 		}
 	}
 	
 	/**
-	 * Description
+	 * メタバイナリからコンテンツelementを作成してVirtualScreenに登録
 	 * @method assignMetaBinary
-	 * @param {} metaData
-	 * @param {} contentData
+	 * @param {JSON} metaData
+	 * @param {String} contentData
 	 */
 	function assignMetaBinary(metaData, contentData) {
 		var previewArea = document.getElementById('preview_area'),
@@ -145,64 +185,335 @@
 		
 		console.log("id=" + metaData.id);
 
-		if (metaData.type === 'text') {
-			tagName = 'pre';
-		} else {
-			tagName = 'img';
-		}
-		if (document.getElementById(metaData.id)) {
-			elem = document.getElementById(metaData.id);
-		} else {
-			elem = document.createElement(tagName);
-			elem.id = metaData.id;
-			elem.style.position = "absolute";
-			previewArea.appendChild(elem);
-		}
-		if (metaData.type === 'text') {
-			// contentData is text
-			elem.innerHTML = contentData;
-		} else {
-			// contentData is blob
-			if (metaData.hasOwnProperty('mime')) {
-				mime = metaData.mime;
+		if (metaData.type === windowType || (metaData.hasOwnProperty('visible') && metaData.visible === "true")) {
+			metaDataDict[metaData.id] = metaData;
+			if (metaData.type === 'text') {
+				tagName = 'pre';
+			} else {
+				tagName = 'img';
 			}
-			blob = new Blob([contentData], {type: mime});
-			if (elem && blob) {
-				elem.src = URL.createObjectURL(blob);
+			if (document.getElementById(metaData.id)) {
+				elem = document.getElementById(metaData.id);
+			} else {
+				elem = document.createElement(tagName);
+				elem.id = metaData.id;
+				elem.style.position = "absolute";
+				
+				insertElementWithDictionarySort(previewArea, elem);
+				//previewArea.appendChild(elem);
 			}
+			if (metaData.type === 'text') {
+				// contentData is text
+				elem.innerHTML = contentData;
+			} else {
+				// contentData is blob
+				if (metaData.hasOwnProperty('mime')) {
+					mime = metaData.mime;
+				}
+				blob = new Blob([contentData], {type: mime});
+				if (elem && blob) {
+					elem.src = URL.createObjectURL(blob);
+				}
+			}
+			vsutil.assignMetaData(elem, metaData, false);
 		}
-		assignMetaData(elem, metaData);
 	}
 	
 	/**
-	 * Description
-	 * @method onmessage
-	 * @param {String} message
+	 * 指定されたメタデータをvisibleにする
+	 * @method setVisibleWindow
+	 * @param {JSON} metaData
 	 */
-	client.onmessage = function (message) {
-		var json;
-		//console.log('> got message');
-		if (typeof message.data === "string") {
-			if (message.data === "update") {
-				// recieve update request
-				console.log("update");
-				updateType = 'all';
-				update();
-			} else if (message.data.indexOf("updateTransform:") >= 0) {
-				// recieve update transfrom request
-				//console.log("updateTransform");
-				updateType = 'transform';
-				update();
+	function setVisibleWindow(metaData) {
+		if (metaData.hasOwnProperty('visible')) {
+			console.log("setVisibleWindow:", metaData);
+			if (metaData.visible === "true") {
+				document.getElementById('preview_area').style.display = "block";
 			} else {
-				// recieve metadata
-				json = JSON.parse(message.data);
-				assignMetaData(document.getElementById(json.id), json);
+				document.getElementById('preview_area').style.display = "none";
 			}
-		} else if (message.data instanceof Blob) {
-			//console.log("found blob");
-			metabinary.loadMetaBinary(message.data, function (metaData, contentData) {
-				assignMetaBinary(metaData, contentData);
-			});
+		}
+	}
+	
+	/**
+	 * VirtualDisplay更新
+	 * @method updateWindow
+	 * @param {JSON} metaData VirtualDisplayメタデータ
+	 */
+	function updateWindow(metaData) {
+		var cx = parseFloat(metaData.posx, 10),
+			cy = parseFloat(metaData.posy, 10),
+			w = parseFloat(metaData.width),
+			h = parseFloat(metaData.height),
+			orgW = parseFloat(vscreen.getWhole().orgW),
+			scale = orgW / w;
+		
+		console.log("scale:" + scale);
+		
+		// scale
+		vscreen.setWholePos(0, 0);
+		vscreen.setWholeCenter(0, 0);
+		vscreen.setWholeScale(scale);
+		
+		// trans
+		vscreen.translateWhole(-cx, -cy);
+	}
+	
+	/**
+	 * リサイズに伴うDisplayの更新
+	 * @method resizeViewport
+	 * @param {JSON} windowData
+	 */
+	function resizeViewport(windowData) {
+		var wh = getWindowSize(),
+			cx = wh.width / 2.0,
+			cy = wh.height / 2.0,
+			scale,
+			id,
+			metaTemp;
+
+		updateWindow(windowData);
+		
+		for (id in metaDataDict) {
+			if (metaDataDict.hasOwnProperty(id)) {
+				if (document.getElementById(id)) {
+					vsutil.assignMetaData(document.getElementById(id), metaDataDict[id], false);
+				} else {
+					delete metaDataDict[id];
+				}
+			}
+		}
+	}
+	
+	window.document.addEventListener("mousedown", function () {
+		var displayArea = document.getElementById('displayid_area');
+		if (displayArea.style.display !== "none") {
+			displayArea.style.display = "none";
+		}
+	});
+	
+	/**
+	 * Description
+	 * @method showDisplayID
+	 * @param {} id
+	 */
+	function showDisplayID(id) {
+		console.log("showDisplayID:" + id);
+		if (id && windowData.id === id) {
+			document.getElementById('displayid_area').style.display = "block";
+			setTimeout(function () {
+				document.getElementById('displayid_area').style.display = "none";
+			}, 8 * 1000);
+		} else if (id === "") {
+			document.getElementById('displayid_area').style.display = "block";
+			setTimeout(function () {
+				document.getElementById('displayid_area').style.display = "none";
+			}, 8 * 1000);
+		}
+	}
+	
+	
+	function assingVisible(json) {
+		var elem = document.getElementById("preview_area");
+		if (elem) {
+			if (isVisible(json)) {
+				console.log("isvisible");
+				vsutil.assignMetaData(elem, json, false);
+				elem.style.display = "block";
+			} else {
+				console.log("not isvisible");
+				elem.style.display = "none";
+			}
+		}
+	}
+
+	function changeID(e) {
+		var elem = document.getElementById('input_id'),
+			val,
+			url;
+		e.preventDefault();
+		if (elem && elem.value) {
+			console.log(elem.value);
+			val = elem.value.split(' ').join('');
+			val = val.split('　').join('');
+			location.hash = fixedEncodeURIComponent(val);
+			location.reload(true);
+		}
+	}
+	
+
+	function updateVisible(json) {
+		var elem = document.getElementById(json.id);
+		//console.log(elem);
+		if (elem) {
+			if (isVisible(json)) {
+				vsutil.assignMetaData(elem, json, false);
+				elem.style.display = "block";
+			} else {
+				elem.style.display = "none";
+			}
+		} else if (isVisible(json)) {
+			// new visible content
+			updateType = 'all';
+			update();
+		}
+		resizeViewport(windowData);
+	}
+	
+	doneAddWindow = function (err, json) {
+		metaDataDict[json.id] = json;
+
+		console.log("doneAddWindow", json);
+		windowData = json;
+		saveCookie();
+		window.parent.document.title = "Display ID:" + json.id;
+		document.getElementById('input_id').value = json.id;
+		document.getElementById('displayid').innerHTML = "ID:" + json.id;
+		updateWindow(windowData);
+		assingVisible(windowData);
+		updateVisible(json);
+	};
+	
+	doneGetWindow = function (err, json) {
+		metaDataDict[json.id] = json;
+
+		console.log("doneGetWindow", json);
+		windowData = json;
+		saveCookie();
+		console.log(windowData);
+		setVisibleWindow(windowData);
+		resizeViewport(windowData);
+		assingVisible(windowData);
+		//updateVisible(json);
+	};
+	
+	doneGetContent = function (err, data) {
+		var metaData = data.metaData,
+			contentData = data.contentData;
+		
+		//console.log("doneGetContent", metaData, contentData);
+		assignMetaBinary(metaData, contentData);
+	};
+	
+	doneGetMetaData = function (err, json) {
+		metaDataDict[json.id] = json;
+		var elem = document.getElementById(json.id);
+		console.log("doneGetMetaData");
+		console.log(elem);
+		if (elem) {
+			if (isVisible(json)) {
+				vsutil.assignMetaData(elem, json, false);
+				elem.style.display = "block";
+			} else {
+				elem.style.display = "none";
+			}
+		} else if (isVisible(json)) {
+			// new visible content
+			updateType = 'all';
+			update();
+		}
+		if (json.type === windowType) {
+			resizeViewport(windowData);
 		}
 	};
-}(window.metabinary));
+
+	function reconnect() {
+		//client = new WebSocket("ws://" + location.hostname + ":8081/v1/");
+		connector.connect(function () {
+			if (!windowData) {
+				console.log("registerWindow");
+				registerWindow();
+			}
+		}, (function () {
+			return function (ev) {
+				console.log('close');
+				setTimeout(function () {
+					reconnect();
+				}, reconnectTimeout);
+			};
+		}()));
+
+		connector.on("update", function (data) {
+			console.log("update");
+			updateType = 'all';
+			update();
+		});
+
+		connector.on("updateWindow", function (data) {
+			updateType = 'window';
+			console.log("updateWindow");
+			update();
+		});
+		
+		connector.on("showWindowID", function (data) {
+			showDisplayID(data.id);
+		});
+		
+		connector.on("updateTransform", function (data) {
+			updateType = 'transform';
+			console.log("updateTransform", data);
+			update(data.id);
+		});
+	}
+
+	/// initialize.
+	/// setup gui events
+	/**
+	 * Description
+	 * @method init
+	 */
+	function init() {
+		var input_id = document.getElementById('input_id'),
+			registered = false,
+			onfocus = false,
+			hideMenuFunc = function () {
+				console.log("onfocus:", onfocus);
+				if (!onfocus) {
+					console.log("hideMenuFunc");
+					document.getElementById('menu').classList.add('hide');
+				}
+				registered = false;
+			};
+		
+		reconnect();
+		
+		// resize event
+		window.onresize = function () {
+			if (timer) {
+				clearTimeout(timer);
+			}
+			timer = setTimeout(function () {
+				registerWindow();
+			}, 200);
+		};
+
+		window.addEventListener('mousemove', function (evt) {
+			document.getElementById('menu').classList.remove('hide');
+			if (!registered) {
+				registered = true;
+				setTimeout(hideMenuFunc, 3000);
+			}
+		});
+		
+		document.getElementById('change_id').onclick = changeID;
+		
+		input_id.onfocus = function (ev) {
+			console.log("onfocus");
+			onfocus = true;
+			document.getElementById('menu').classList.remove('hide');
+			clearTimeout(hideMenuFunc);
+		};
+		input_id.onblur = function (ev) {
+			console.log("onblur");
+			onfocus = false;
+		};
+		input_id.onkeypress = function (ev) {
+			console.log(ev.keyCode);
+			if (ev.keyCode === 13) { // enter
+				document.getElementById('change_id').onclick(ev);
+			}
+		};
+	}
+	
+	window.onload = init;
+}(window.vscreen, window.vscreen_util, window.ws_connector));
