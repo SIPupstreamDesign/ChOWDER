79c0e73648a40b2a068f4640d426335f808fe761
masahiro sugimoto <masahiro-s@digirea.com>
2016-09-02 11:17:33 +0900

    add search tab in checkbox and other

--- client/js/group_box.js
+++ client/js/group_box.js
@@ -75,7 +75,6 @@
 				
 				this.tabGroupToElems[groupName].push(box);
 			}
-
 			// 追加ボタン
 			elem = document.createElement('div');
 			elem.className = "group_tab_append";
@@ -85,8 +84,10 @@
 			elem.appendChild(span);
 			elem.onclick = function (evt) {
 				alert("not implemented");
-			}
+			};
 			tabArea.appendChild(elem);
+            // search tab generate
+            this.gen_search_tab_box();
 		}
 	};
 
@@ -198,12 +199,76 @@
 		}
 	};
 
+    GroupBox.prototype.gen_search_tab_box = function(){
+        var e, f, g, h, i, j;
+        var box = document.getElementById('search_tab_box');
+        // タブの中の要素全体を包むラッパー
+        e = document.createElement('div');
+        e.style.display = 'flex';
+        e.style.flexDirection = 'row';
+        box.appendChild(e);
+        // 検索窓とチェックボックスの入る左側のカラム
+        f = document.createElement('div');
+        f.style.width = '300px';
+        f.style.height = '100%';
+        f.style.display = 'flex';
+        f.style.flexDirection = 'column';
+        e.appendChild(f);
+        // アイテムが並ぶ右側のカラム
+        g = document.createElement('div');
+        g.style.width = 'calc(100% - 300px)';
+        g.style.height = '100%';
+        e.appendChild(g);
+        // 左カラム内、上段に検索ボックス
+        h = document.createElement('input');
+        h.type = 'text';
+        h.style.display = 'block';
+        h.style.width = '100%';
+        f.appendChild(h);
+        // 左カラム内、下段にチェックボックスが入るエリア
+        h = document.createElement('div');
+        h.style.display = 'block';
+        h.style.width = '98%';
+        h.style.padding = '1%';
+        f.appendChild(h);
+
+        // temp そのいち
+        for(i = 0; i < 5; i++){
+            j = document.createElement('div');
+            e = document.createElement('input');
+            e.id = 'check_temp_' + i;
+            e.type = 'checkbox';
+            e.style.display = 'inline-block';
+            e.style.margin = '10px';
+            j.appendChild(e);
+            f = document.createElement('label');
+            f.setAttribute('for', 'check_temp_' + i);
+            f.textContent = 'check_temp_' + i;
+            f.style.color = 'white';
+            f.style.margin = '10px 5px';
+            f.style.cursor = 'pointer';
+            j.appendChild(f);
+            h.appendChild(j);
+        }
+
+        // temp そのに
+        for(i = 0; i < 3; i++){
+            e = document.createElement('div');
+            e.style.display = 'inline-block';
+            e.style.margin = '20px';
+            e.style.width = '150px';
+            e.style.height = '150px';
+            e.style.backgroundColor = 'skyblue';
+            g.appendChild(e);
+        }
+    };
+
 	function init(containerElem, setting) {
 		return new GroupBox(containerElem, setting);
 	}
 
 	function is_active(tabID) {
- 		var tab = document.getElementById(tabID);
+		var tab = document.getElementById(tabID);
 		return tab.className.indexOf('active') >= 0;
 	}
 
