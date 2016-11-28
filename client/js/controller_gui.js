fb390e8bac64fd5eb0a09700c4e0d0e221a4f960
uimac <tori31001@gmail.com>
2016-11-28 10:47:48 +0900

    open random id view

--- client/js/controller_gui.js
+++ client/js/controller_gui.js
@@ -668,6 +668,15 @@
 		};
 	}
 
+	function guid10() {
+		function s4() {
+			return Math.floor((1 + Math.random()) * 0x10000)
+			.toString(16)
+			.substring(1);
+		}
+		return s4() + s4() + s4().slice(-2);
+	}
+
 	/**
 	 * 初期化
 	 * @method init
@@ -683,7 +692,10 @@
 				menu : [{
 					Controller : [{
 							Display : {
-								url : "view.html"
+								func : function () {
+									var viewURL = "view.html#" + guid10();
+									window.open(viewURL);
+								}
 							},
 						}],
 					url : "controller.html"
