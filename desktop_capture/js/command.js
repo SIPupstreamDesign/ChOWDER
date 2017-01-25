13e3a07cf2333bc5648bc9124fe494b2e92f52da
Hayato Izumi <hayto-i@digirea.com>
2017-01-25 16:35:49 +0900

    add files

---
+++ desktop_capture/js/command.js
@@ -0,0 +1,40 @@
+/*jslint devel:true*/
+/*global require, socket, module, Buffer */
+
+(function () {
+	"use strict";
+	
+	var Command = {
+		// request command
+		AddContent : "AddContent",
+		AddMetaData : "AddMetaData",
+		AddWindowMetaData : "AddWindowMetaData",
+		AddGroup : "AddGroup",
+		DeleteGroup : "DeleteGroup",
+		GetContent : "GetContent",
+		GetMetaData : "GetMetaData",
+		GetWindowMetaData : "GetWindowMetaData",
+		UpdateVirtualDisplay : "UpdateVirtualDisplay",
+		GetVirtualDisplay : "GetVirtualDisplay",
+		GetGroupList : "GetGroupList",
+		
+		// using both server and client
+		Update : "Update",
+		UpdateMetaData : "UpdateMetaData",
+		UpdateContent : "UpdateContent",
+		UpdateWindowMetaData : "UpdateWindowMetaData",
+		UpdateGroup : "UpdateGroup",
+		ChangeGroupIndex : "ChangeGroupIndex",
+		DeleteContent : "DeleteContent",
+		DeleteWindowMetaData : "DeleteWindowMetaData",
+		ShowWindowID : "ShowWindowID",
+		UpdateMouseCursor : "UpdateMouseCursor",
+		
+		SendMessage: "SendMessage",
+
+		// to client
+		Disconnect : "Disconnect"
+	};
+	
+	window.command = Command;
+}());
