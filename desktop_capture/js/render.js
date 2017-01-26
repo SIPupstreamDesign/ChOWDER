10545f9457a0ca1a26966cc8c4c7dee6da429614
Hayato Izumi <hayto-i@digirea.com>
2017-01-26 19:41:23 +0900

    still can't send image

--- desktop_capture/js/render.js
+++ desktop_capture/js/render.js
@@ -1,5 +1,7 @@
 "use strict";
 const electron = require('electron');
+let metabinary = require('./js/metabinary.js');
+let ws_connector = require('./js/ws_connector.js')
 
 // モジュールの追加
 const desktopCapturer = electron.desktopCapturer;
@@ -14,18 +16,18 @@ window.URL = window.URL || window.webkitURL;
 
 (function(){
     
-    captureInit();
+    initCapturing();
 
-    function captureInit(){
+    function initCapturing(){
         desktopCapturer.getSources({types: ['window', 'screen']}, function(error, sources) {
             if (error) throw error;
             for (let i = 0; i < sources.length; ++i) {
                 console.log("sources["+i+"].name = "+ sources[i].name);
                 console.log(sources);
-                if(sources[i].name != "Entire screen" && sources[i].name != "electron-capture") {
+                if(sources[i].name != "electron-capture") {
                     addImage(sources[i].thumbnail);
                 }
-                // ストリーミング。後ほどアクティブなウィンドウを対象に切り替える。
+                // ストリーミング。後ほどアクティブなウィンドウを対象に切り替えていく。
                 if (sources[i].name == "Entire screen") {
                     navigator.getUserMedia({
                         audio: false,
@@ -46,30 +48,34 @@ window.URL = window.URL || window.webkitURL;
         });
     }
 
-    // canvas2dへ書き出す行程
+    // canvas2dへイメージとして
+    let video = document.getElementById('world');
+    let canvas = document.getElementById('canvas');
+    let ctx = canvas.getContext("2d");
+    
+    ctx.width = 640;
+    ctx.height = 450;
+
     let cap = false;
     let capButton = document.getElementById('capture');
+    let drawInterval;
 
     capButton.addEventListener('click',function(eve){
         // フラグがオフであれば
         if(cap === false){
             cap = true;
             capButton.value = "Capture Stop";
-            let video = document.getElementById('world');
-            let canvas = document.getElementById('canvas');
-            let ctx = canvas.getContext("2d");
-            ctx.width = 250;
-            ctx.height = 250;
-            setInterval(function(){
-                ctx.drawImage(video, 0, 0, 250, 250);
-            },2000);
+            drawInterval = setInterval(drawImage(),2000);
         }
         // フラグがオンであれば
         else if(cap === true){
             cap = false;
             capButton.value = "Capture Start";
+            clearInterval(drawInterval);
+            ctx.clearRect(0, 0, ctx.width, ctx.height);
         }
-    },false)
+    },false);
+
 
     function addImage(image) {
         const elm = document.createElement("img");
@@ -79,14 +85,32 @@ window.URL = window.URL || window.webkitURL;
         document.body.appendChild(elm);
     }
 
-    function draw(){
-        ctx.drawImage(video, 0, 0, 250, 250);
+    function drawImage(){     
+        ctx.drawImage(video, 0, 0, 960, 540, 0, 0, ctx.width, ctx.height);
     }
+
 
     function gotStream(stream) {
         document.querySelector('video').src = URL.createObjectURL(stream);
     }
 
+    function replaceFocus(sources1, sources2){
+        let tmpId;
+        let tmpName;
+        let tmpThumbnail;
+
+        tmpId = sources1.id;
+        tmpName = source1.name;
+        tmpThumbnail = source1.thumbnail;
+
+        source1.id = source2.id;
+        source1.name = source2.name;
+        source1.thumbnail = source2.thumbnail;
+
+        source2.id = tmpId;
+        source2.name = tmpName;
+        source2.thumbnail = tmpThumbnail;
+    }
 
     function getUserMediaError(e) {
         console.log('getUserMediaError');
