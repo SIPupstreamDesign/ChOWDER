ab175a125a88554dbf90e429ad708beb23695fbe
uimac <tori31001@gmail.com>
2017-02-01 20:11:49 +0900

    add backup/restore scripts

---
+++ redis/backup.js
@@ -0,0 +1,71 @@
+/**
+ * Choder用DBバックアップスクリプト
+ * 使用方法:
+ *    redisを起動している状態で、
+ *    node backup.js
+ *    を実行し、表示されるChOWDER DBから、バックアップしたいDBの番号を入力しEnter.
+ *    Backup finishedと表示されると正常終了です.
+ */
+
+var redis = require("redis");
+var BSON = require('bson');
+var bson = new BSON();
+var client = redis.createClient(6379, '127.0.0.1', {'return_buffers': true });
+var textclient = redis.createClient(6379, '127.0.0.1', {'return_buffers': false});
+var fs = require("fs");
+
+client.hgetall("tiled_server:t:dblist", function (err, dblist) {
+	var i;
+	console.log("Please select backup db");
+	for (i = 0; i < Object.keys(dblist).length; ++i) {
+		console.log((i + 1) +": " + Object.keys(dblist)[i]);
+	}
+	var reader = require('readline').createInterface({
+		input: process.stdin,
+		output: process.stdout
+	});
+	reader.on('line', function (line) {
+		var num = Number(line);
+		var i;
+		if (num !== null && !isNaN(num)) {
+			var dbindex = num - 1;
+			var dbname = Object.keys(dblist)[dbindex];
+			var dbid = dblist[dbname];
+			if (dbid) {
+				var target = "tiled_server:t:" + dbid + ":*";
+				console.log("Backup started:" + dbname, target);
+
+				textclient.keys(target, (function (dbid, dbname) {
+					return function (err, keys) {
+						var i,
+							backupData = {};
+							
+						if (!err) {
+											
+							// DBリスト復元用エントリ.
+							var entry = {};
+							entry.id = dbid;
+							entry.name = dbname;
+							backupData["dbentry"] = entry;
+							
+							// 実エントリ
+							for (i = 0; i < keys.length; i = i + 1) {
+								client.dump(keys[i], (function (isLast, key) {
+									return function (err, result) {
+										backupData[key] = result;
+										if (isLast) {
+											fs.writeFileSync(dbname + ".backup", bson.serialize(backupData));
+											console.log("Backup finished");
+											process.exit();
+										}
+									};
+								}(i === keys.length - 1, keys[i])));
+							}
+						}
+					};
+				}(dbid, dbname)));
+			}
+		}
+	});
+	reader.on('close', function () {});
+});
---
+++ redis/restore.js
@@ -0,0 +1,99 @@
+/**
+ * Choder用DBリストアスクリプト
+ * 使用方法:
+ *    redisを起動している状態で、
+ *    node restore.js バックアップファイルパス
+ *    を実行し、Restore Endと表示されると正常終了です.
+ *    既存DBで同じ名前のものがあった場合, 上書きされます
+ */
+
+var redis = require("redis");
+var BSON = require('bson');
+var bson = new BSON();
+var path = require('path');
+var fs = require('fs');
+var client = redis.createClient(6379, '127.0.0.1', { 'return_buffers': true });
+var textClient = redis.createClient(6379, '127.0.0.1', {'return_buffers': false});
+
+// DBリストに復元したDBが無い場合は追加しておく
+// @param key DB内の何かのkey
+function appendDBtoDBList(dbentry, endCallback) {
+	var dbid = key.split(':')[2];
+	textClient.hgetall("tiled_server:t:dblist", function (err, reply) {
+		if (!err) {
+			textClient.hmset("tiled_server:t:dblist", dbentry.name, String(dbentry.id), endCallback);
+		} else {
+			endCallback(err)
+		}
+	});
+}
+
+if (process.argv.length > 2) {
+	var backupFile = path.resolve(".", process.argv[2]);
+	var backup = fs.readFileSync(backupFile);
+	var data = bson.deserialize(backup);
+	console.log(Object.keys(data));
+	var key;
+	var dbentry = {};
+	if (data.hasOwnProperty("dbentry")) {
+		dbentry = data.dbentry;
+		delete data.dbentry;
+	} else {
+		console.error("not found db");
+		process.exit(-1);
+	}
+	var i = 0;
+	for (; i < Object.keys(data).length; i = i + 1) {
+		key = Object.keys(data)[i];
+		var buffer = data[key].buffer;
+		textClient.exists(key, (function (isLast, key, buffer, dbentry) {
+			return function (err, doesExist) {
+				if (!err &&  doesExist === 1) {
+					// 削除してからrestore
+					client.del(key, function (err) {
+						client.restore(key, 0, buffer, function (err, reply) {
+							if (err) {
+								console.error(err, key);
+								process.exit(-1);
+							}
+							if (isLast) {
+								appendDBtoDBList(dbentry, function (err) {
+									if (!err) {
+										console.log("Restore End");
+										process.exit(0);
+									} else {
+										console.error(err);
+										process.exit(-1);
+									}
+								});
+							}
+						});
+					});
+				} else {
+					// そのままrestore
+					client.restore(key, 0, buffer, function (err, reply) {
+						if (err) {
+							console.error(err, key);
+							process.exit(-1);
+						}
+						if (isLast) {
+							appendDBtoDBList(dbentry, function (err) {
+								if (!err) {
+									console.log("Restore End");
+									process.exit(0);
+								} else {
+									console.error(err);
+									process.exit(-1);
+								}
+							});
+						}
+					});
+				}
+			};
+		}(i === Object.keys(data).length - 1, key, buffer, dbentry)));
+	}
+} else {
+	console.log("usage: ");
+	console.log("> node restore.js default.backup");
+	process.exit(0);
+}
