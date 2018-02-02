54c07c340dcf431bd9151ec1ef82eeb8fa7bbcfc
uimac <tori31001@gmail.com>
2018-02-02 17:43:20 +0900

    wip capture extension

---
+++ chrome_extension_for_webrtc/js/app.js
@@ -0,0 +1,49 @@
+(function () {
+
+	window.onload = function () {
+
+		//const EXTENSION_ID = 'elpljfkdapfmcbhlkidibdpheipocfgo';
+		const video = document.getElementById('screen-view');
+		const start_button = document.getElementById('start_button');
+		const stop_button = document.getElementById('stop_button');
+		const request = { sources: ['screen', 'window', 'tab'] };
+		
+		let stream;
+		start_button.addEventListener('click', function (evt) {
+			const EXTENSION_ID = document.getElementById('extensionid_input').value;
+
+			chrome.runtime.sendMessage(EXTENSION_ID, request, function (response) {
+				console.log(response)
+				if (response && response.type === 'success') {
+					navigator.mediaDevices.getUserMedia({
+						video: {
+							mandatory: {
+								chromeMediaSource: 'desktop',
+								chromeMediaSourceId: response.streamId,
+							}
+						}
+					}).then(returnedStream => {
+						stream = returnedStream;
+						video.src = URL.createObjectURL(stream);
+						start_button.style.display = "none";
+						stop_button.style.display = "inline";
+					}).catch(err => {
+						console.error('Could not get stream: ', err);
+					});
+				} else {
+					console.error('Could not get stream');
+				}
+			});
+		});
+		stop_button.addEventListener('click', function (evt) {
+			stream.getTracks().forEach(function (track) {
+				track.stop()
+			});
+			URL.revokeObjectURL(video.src);
+			video.src = '';
+			stop_button.style.display = "none";
+			start_button.style.display = "inline";
+		});
+	};
+
+}());
---
+++ chrome_extension_for_webrtc/js/background.js
@@ -0,0 +1,36 @@
+(function () {
+	"use strict";
+
+	console.log("this is background")
+
+	let onMessageFunc = (message, sender, sendResponse) => {
+	   const sources = message.sources;
+	   const tab = sender.tab;
+	   chrome.desktopCapture.chooseDesktopMedia(sources, tab, (streamId) => {
+		   if (!streamId) {
+			   sendResponse({
+				   type: 'error',
+				   message: 'Failed to get stream ID'
+			   });
+		   } else {
+			   sendResponse({
+				   type: 'success',
+				   streamId: streamId
+			   });
+		   }
+	   });
+	   return true;
+   	};
+
+	chrome.runtime.onMessage.addListener(onMessageFunc);
+	chrome.runtime.onMessageExternal.addListener(onMessageFunc);
+	
+	// for test
+	chrome.browserAction.onClicked.addListener(function () {
+		chrome.windows.create({
+			url: "index.html",
+			focused: true,
+			type: "popup"
+		});
+	});
+}());
