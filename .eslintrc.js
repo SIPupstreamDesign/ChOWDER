96c8361849eb3688790d4fa2d67e8e03c7a3d287
Yutaka Obuchi <yutaka.obuchi@gmail.com>
2018-06-15 11:41:37 +0900

    added eslint and vscode settings

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "env": {
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "no-console": 0,
+        "indent": [ "error", "tab" ],
+        "linebreak-style": [ "error", "windows" ],
+        "quotes": [ "error", "single" ],
+        "semi": [ "error", "always" ],
+        "no-unused-vars": [ "warn", { "args": "none" } ]
+    }
+};
