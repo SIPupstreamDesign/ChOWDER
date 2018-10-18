728422ff08a8945594836efbd4c0f53f18fc6565
KazumaHatta <tori31001@gmail.com>
2018-10-18 15:50:41 +0900

    change conf.json parameter of standalone-electron

--- standalone-electron/index.js
+++ standalone-electron/index.js
@@ -42,22 +42,22 @@ function createWindows() {
 
 		var windowProps = CONFIG.windows[id];
 
-		if (!windowProps.rect) {
-			console.error('conf.json is invalid: There are no required parameter "rect" for window "' + id + '"');
+		if (!windowProps.position) {
+			console.error('conf.json is invalid: There are no required parameter "position" for window "' + id + '"');
 			return;
 		}
 
-		if (windowProps.rect.length !== 4) {
-			console.error('conf.json is invalid: The parameter "rect" for window "' + id + '" is invalid');
+		if (!windowProps.size) {
+			console.error('conf.json is invalid: There are no required parameter "size" for window "' + id + '"');
 			return;
 		}
 
 		var frame = typeof windowProps.frame !== 'undefined' ? windowProps.frame : true;
 		var window = new electron.BrowserWindow({
-			x: windowProps.rect[0],
-			y: windowProps.rect[1],
-			width: windowProps.rect[2],
-			height: windowProps.rect[3],
+			x: windowProps.position[0],
+			y: windowProps.position[1],
+			width: windowProps.size[2],
+			height: windowProps.size[3],
 			frame: frame,
 			transparent: !frame,
 			toolbar: false,
@@ -68,8 +68,8 @@ function createWindows() {
 		var query = mapToQueryString({
 			id: id || undefined,
 			group: windowProps.group || undefined,
-			posx: windowProps.position ? windowProps.position[0] : undefined,
-			posy: windowProps.position ? windowProps.position[1] : undefined,
+			posx: windowProps.vda_position ? windowProps.vda_position[0] : undefined,
+			posy: windowProps.vda_position ? windowProps.vda_position[1] : undefined,
 			scale: windowProps.scale || undefined
 		});
 		
