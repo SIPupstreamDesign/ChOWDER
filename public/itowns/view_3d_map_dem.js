02b9db41c65d84b8b9d0677b187db30cfeff9cee
KazumaHatta <tori31001@gmail.com>
2020-01-06 19:19:35 +0900

    for iTowns dem(wip

--- public/itowns/view_3d_map_dem.js
+++ public/itowns/view_3d_map_dem.js
@@ -1,8 +1,9 @@
 
 
-window.onload = function() {
-    
-    class MyWMTSSource extends itowns.WMTSSource {
+window.onload = function () {
+
+
+    class MyWMTSSource extends itowns.TMSSource {
         /**
          * @param {Object} source - An object that can contain all properties of a
          * WMTSSource and {@link Source}. Only `url` and `name` are mandatory.
@@ -11,13 +12,24 @@ window.onload = function() {
          */
         constructor(source) {
             super(source);
+            this.isInverted = true;
+            super.isInverted = true;
             this.url = "https://cyberjapandata.gsi.go.jp/xyz/dem_png/%TILEMATRIX/%COL/%ROW.png"
         }
 
         urlFromExtent(extent) {
-            console.error("urlFromExtent", extent)
-            return super.urlFromExtent(extent);
+            let newExtent = JSON.parse(JSON.stringify(extent));
+            console.log("urlFromExtent", extent, newExtent)
+            return super.urlFromExtent(newExtent);
         }
+
+        /*
+        extentInsideLimit(extent) {
+            let result = super.extentInsideLimit(extent);
+            console.error("extentInsideLimit", result);
+            return result;
+        }
+        */
     }
 
     class MyTMSSource extends itowns.TMSSource {
@@ -33,18 +45,18 @@ window.onload = function() {
         }
 
         urlFromExtent(extent) {
-            console.error("STD urlFromExtent", extent)
+            //console.log("STD urlFromExtent", extent)
             return super.urlFromExtent(extent);
         }
     }
 
     // for measure performance
     var startTime = performance.now();
-    
+
     // # Simple Globe viewer
     // Define initial camera position
-    var placement  = { 
-        coord : new itowns.Coordinates("EPSG:4326", 139.839478, 35.652832, 0),
+    var placement = {
+        coord: new itowns.Coordinates("EPSG:4326", 139.839478, 35.652832, 0),
         range: 250000
     };
 
@@ -52,47 +64,88 @@ window.onload = function() {
     var viewerDiv = document.getElementById('viewerDiv');
     var miniDiv = document.getElementById('miniDiv');
     // Instanciate iTowns GlobeView*
-    var view = new itowns.GlobeView(viewerDiv, placement );
+    var view = new itowns.GlobeView(viewerDiv, placement);
     setupLoadingScreen(viewerDiv, view);
-    
+
     // This layer is defined in a json file but it could be defined as a plain js
     // object. See Layer* for more info.
     itowns.Fetcher.json('./layers/JSONLayers/OPENSM.json').then(function _(config) {
         config.source = new MyTMSSource(config.source);
         var layer = new itowns.ColorLayer('OPENSM', config);
-        view.addEventListener(itowns.VIEW_EVENTS.LAYERS_INITIALIZED, () =>{
+        view.addEventListener(itowns.VIEW_EVENTS.LAYERS_INITIALIZED, () => {
             console.log("loaded")
         });
-        
+
         view.addLayer(layer).then(menuGlobe.addLayerGUI.bind(menuGlobe));
     });
 
     // Add two elevation layers.
     // These will deform iTowns globe geometry to represent terrain elevation.
     function addElevationLayerFromConfig(config) {
+        const getTextureFloat = function getTextureFloat(buffer) {
+            return new itowns.THREE.DataTexture(buffer, 256, 256, itowns.THREE.AlphaFormat, itowns.THREE.FloatType);
+        };
+        const textureLoader = new itowns.THREE.TextureLoader();
+        let texture2D = function (url, options = {}) {
+            let res;
+            let rej;
+            textureLoader.crossOrigin = options.crossOrigin;
+            const promise = new Promise((resolve, reject) => {
+                res = resolve;
+                rej = reject;
+            });
+            textureLoader.load(url, res, () => { }, rej);
+            return promise;
+        }
+
+        console.error(config)
+        config.source.fetcher = function (url, options = {}) {
+            return texture2D(url, options).then((tex) => { // bufferはpng画像
+                if (window.isDownload)
+                {
+                    download(tex.image.src);
+                }
+
+                let canvas = document.createElement("canvas");
+                canvas.width = "256";
+                canvas.height = "256";
+                let context = canvas.getContext('2d');
+                context.drawImage(tex.image, 0, 0);
+                let pixData = context.getImageData(0, 0, 256, 256).data;
+                let alt;
+                let heights = [];
+                for (let y = 0; y < 256; y++) {
+                    for (let x = 0; x < 256; x++) {
+                        let addr = (x + y * 256) * 4;
+                        let R = pixData[addr];
+                        let G = pixData[addr + 1];
+                        let B = pixData[addr + 2];
+                        //let A = pixData[addr + 3];
+                        if (R == 128 && G == 0 && B == 0) {
+                            alt = 0;
+                        } else {
+                            alt = (R * 65536 + G * 256 + B);
+                            if (alt > 8388608) {
+                                alt = (alt - 16777216);
+                            }
+                            alt = alt * 0.01;
+                        }
+                        heights.push(alt);
+                    }
+                }
+
+                const floatArray = new Float32Array(heights);
+                const texture = getTextureFloat(floatArray);
+                return texture;
+            });
+        }
+
         config.source = new MyWMTSSource(config.source);
 
         let layer = new itowns.ElevationLayer(config.id, config);
         view.addLayer(layer).then(menuGlobe.addLayerGUI.bind(menuGlobe));
-        layer.scale = 10.0;
+        layer.scale = 1.0;
     }
-
-    let textureFloatOrg = itowns.Fetcher.textureFloat;
-
-    /*
-    const arrayBuffer = (url, options = {}) => fetch(url, options).then((response) => {
-        //checkResponse(response);
-        return response.arrayBuffer();
-    });
-    
-    let textureFloat = (url, options = {}) => {
-        return arrayBuffer(url, options).then((buffer) => {
-            const floatArray = new Float32Array(buffer);
-            const texture = getTextureFloat(floatArray);
-            return texture;
-        });
-    };
-    */
 
     itowns.Fetcher.json('./layers/JSONLayers/WORLD_DTM.json').then(addElevationLayerFromConfig);
     //itowns.Fetcher.json('./layers/JSONLayers/IGN_MNT_HIGHRES.json').then(addElevationLayerFromConfig);
