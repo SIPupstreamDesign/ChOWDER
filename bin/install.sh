16f93dbf9c08daa2d0b6ba88f25e1368edbe742b
sokayama <shuhei-o@digirea.com>
2020-02-26 18:45:05 +0900

    setup coturn

--- bin/install.sh
+++ bin/install.sh
@@ -11,7 +11,14 @@ cp src/redis-server ../../redis/
 cd ..
 rm -rf redis-*
 
+curl -O http://turnserver.open-sys.org/downloads/v4.5.1.1/turnserver-4.5.1.1.tar.gz
+tar xf turnserver-4.5.1.1.tar.gz
+cd turnserver-4.5.1.1
+./configure
+make && make install
 cd ..
+rm -rf turnserver-4.5.1.1
+
 npm install
 
 cd $ORIGIN
